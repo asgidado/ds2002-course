4dca7b0351fef69cc3c7f8c3a472eb2c5b19b0cc
asgidado <abdulgidado201@gmail.com>
2026-02-06 09:06:15 -0500

    bash script

--- mywork/lab3/analyze-moby-dick.sh
+++ mywork/lab3/analyze-moby-dick.sh
@@ -1,28 +1,14 @@
 SEARCH_PATTERN=$1
 OUTPUT=$2
 
-curl https://gist.githubusercontent.com/StevenClontz/4445774/raw/1722a289b665d940495645a5eaaad4da8e3ad4c7/mobydick.txt > mobydick.txt
+curl -s https://gist.githubusercontent.com/StevenClontz/4445774/raw/1722a289b665d940495645a5eaaad4da8e3ad4c7/mobydick.txt > mobydick.txt
 
-if [[ -v OUTPUT ]]; then 
-    grep -o SEARCH_PATTERN < mobydick.txt > OUTPUT | wc -l > OCCURENCES
+if [[ -n "$OUTPUT" ]]; then
+    OCCURENCES=$(grep -o "$SEARCH_PATTERN" mobydick.txt | tee "$OUTPUT" | wc -l)
 else
-    grep -o SEARCH_PATTERN < mobydick.txt > results.txt | wc -l > OCCURENCES
+    grep -o "$SEARCH_PATTERN" mobydick.txt > results.txt
+    OCCURENCES=$(wc -l < results.txt)
 fi
 
 echo "The search pattern <$SEARCH_PATTERN> was found <$OCCURENCES> time(s)."
-
-
-# SEARCH_PATTERN=$1
-#OUTPUT=$2
-
-#curl -s https://gist.githubusercontent.com/StevenClontz/4445774/raw/1722a289b665d940495645a5eaaad4da8e3ad4c7/mobydick.txt > mobydick.txt
-
-#if [[ -n "$OUTPUT" ]]; then
-    #OCCURENCES=$(grep -o "$SEARCH_PATTERN" mobydick.txt | tee "$OUTPUT" | wc -l)
-#else
-    #grep -o "$SEARCH_PATTERN" mobydick.txt > results.txt
-    #OCCURENCES=$(wc -l < results.txt)
-#fi
-
-#echo "The search pattern <$SEARCH_PATTERN> was found <$OCCURENCES> time(s)."
 
